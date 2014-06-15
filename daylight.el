251ced168fc65327616a97ccd1bce956057544b4
Stefan Huchler <stefan.huchler@mail.de>
2014-06-16 00:42:22 +0200

    fixes now completly the problem with late-hour after 0 a´clock

--- daylight.el
+++ daylight.el
@@ -94,7 +94,7 @@
          daylight-morning-theme)
         ((and (>= hour daylight-afternoon-hour) (< hour daylight-evening-hour))
          daylight-afternoon-theme)
-	((and (>= hour daylight-evening-hour) (or (< hour daylight-late-hour) (> daylight-evening-hour daylight-late-hour)))
+	((and (or (>= hour daylight-evening-hour) (< hour daylight-morning-hour))(or (< hour daylight-late-hour) (>= daylight-evening-hour daylight-late-hour)))
          daylight-evening-theme)
         (t
          daylight-late-theme)))
